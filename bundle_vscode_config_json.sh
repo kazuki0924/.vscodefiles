d324ab407fc4b5ca1d93d91e2c40e373e2aa9728
Kazuki M <kazuki0924@gmail.com>
2021-06-17 22:14:00 +0900

    Add: json merging script

--- bundle_vscode_config_json.sh
+++ bundle_vscode_config_json.sh
@@ -2,4 +2,17 @@
 set -Eeuo pipefail
 IFS=$'\n\t'
 
-jq -rs 'reduce .[] as $item ({}; . * $item)' **/settings.jsonc > settings.json
+DIR="$HOME/.vscodefiles"
+mapfile -t FILES < <(fd settings.json "${1-$DIR}")
+
+for FILE in "${FILES[@]}"; do
+  strip-json-comments "$FILE" > "$FILE.output"
+done
+
+jq -rs 'reduce .[] as $item ({}; . * $item)' ./**/**/*.output > settings.json
+
+mapfile -t OUTPUTS < <(fd .output "${1-$DIR}")
+
+for OUTPUT in "${OUTPUTS[@]}"; do
+  trash-put -v $OUTPUT
+done
