5c4910e7535e78ad50021ea84c4718e49b275d06
Kazuki M <kazuki0924@gmail.com>
2021-06-17 23:12:10 +0900

    Add: logic for keybindings.json

--- bundle_vscode_config_json.sh
+++ bundle_vscode_config_json.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 IFS=$'\n\t'
+shopt -s globstar
+
+# TODO: requirements, backup original and copy files, extensions, extensions install etc
 
 DIR="$HOME/.vscodefiles"
 mapfile -t FILES < <(fd settings.json "${1-$DIR}")
@@ -9,10 +12,12 @@ for FILE in "${FILES[@]}"; do
   strip-json-comments "$FILE" > "$FILE.output"
 done
 
-jq -rs 'reduce .[] as $item ({}; . * $item)' ./**/**/*.output > settings.json
+jq -rs 'reduce .[] as $item ({}; . * $item)' **/*.output > settings.json
 
 mapfile -t OUTPUTS < <(fd .output "${1-$DIR}")
 
 for OUTPUT in "${OUTPUTS[@]}"; do
   trash-put -v $OUTPUT
 done
+
+jq -rs flatten **/keybindings.json > keybindings.json
