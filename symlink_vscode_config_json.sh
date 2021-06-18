df9147cde40c4522c48742085177cbbb8d726c1c
Kazuki M <kazuki0924@gmail.com>
2021-06-18 13:37:51 +0900

    Add: symlink script

--- symlink_vscode_config_json.sh
+++ symlink_vscode_config_json.sh
@@ -10,15 +10,21 @@ JSON_FILES=(
 )
 
 DIRS=(
-  "$HOME/Library/Application Support/Code - Insiders/User/"
-  "$HOME/Library/Application Support/Code/User/"
+  "$HOME/Library/Application Support/Code - Insiders/User"
+  "$HOME/Library/Application Support/Code/User"
 )
 
 for JSON in "${JSON_FILES[@]}"; do
   for DIR in "${DIRS[@]}"; do
+    [[ ! -d $DIR ]] && continue
     SOURCE="$DOTDIR/$JSON.json"
     LINK="$DIR/$JSON.json"
-    [[ -f $SOURCE ]] && mv "$SOURCE" "$DOTDIR/automatic_backups/$JSON/$JSON_$(date +"%Y_%m_%d").json"
+    BACKUP_FILE="$DOTDIR/automatic_backups/$JSON/"$JSON"_$(date +"%Y_%m_%d").json"
+    if [[ -f BACKUP_FILE ]]; then
+      [[ -f $SOURCE ]] && mv "$LINK" "$DOTDIR/automatic_backups/$JSON/"$JSON"_$(date +"%Y_%m_%d_%H%M%S").json"
+    else
+      [[ -f $SOURCE ]] && mv "$LINK" "$BACKUP_FILE"
+    fi
     if [[ ! -L "$LINK" ]]; then
       ln -sfnv "$SOURCE" "$LINK"
     fi
